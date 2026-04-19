ffae6e36ecb2d371b5023b4a56302f7525529604
qurrotulaini0325 <qurrotulaini0325@gmail.com>
2026-04-20 00:52:19 +0700

    remember token bug fixed

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,6 +37,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password_hash',
+        'remember_token',
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->boolean('is_active')->default(true);
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamp('last_login_at')->nullable();
+            $table->rememberToken();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
 
